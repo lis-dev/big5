def395bdb0905ed8fc2946b6c1e9cab4a80202f5
lis-dev <mathfak@gmail.com>
2021-11-24 12:30:56 +0200

    updated loading component view

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 export function Loading() {
   return (
-    <div className='spinner text-primary' role='status'>
+    <div className='spinner text-primary mx-auto flex p-5 m-5' role='status'>
       <span className='sr-only'>Loading...</span>
     </div>
   );
